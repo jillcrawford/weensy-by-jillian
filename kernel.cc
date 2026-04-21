00af04eb59d1b200cc09ff080ba8215b2efae6aa
jillcrawford <jrcrawford@anderson.edu>
2026-04-21 10:59:52 -0400

    change mapping loop to procstartaddr

--- kernel.cc
+++ kernel.cc
@@ -163,7 +163,7 @@ void process_setup(pid_t pid, const char* program_name) {
 
 // Copy kernel mappings into the new process page table
     for (vmiter it(kernel_pagetable, 0);
-        it.va() < MEMSIZE_PHYSICAL;
+        it.va() < PROC_START_ADDR;
         it += PAGESIZE) {
 
         if (!it.present()) {
